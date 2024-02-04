a83cc2c1796132ebac8a1218c2b57bb3fa6fd600
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-04 10:24:04 +0900

    user登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
+  validates :store_name, presence: true, 
+  validates :store_no,   presence: true, numericality: { only_integer: true, less_than_or_equal_to: 999 }
+  validates :password,   presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/}
 end
