a7c218c14e675beb967f7d6971319e212e4ac88d
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-04 10:29:03 +0900

    userバリデーションカンマ削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   
-  validates :store_name, presence: true, 
+  validates :store_name, presence: true
   validates :store_no,   presence: true, numericality: { only_integer: true, less_than_or_equal_to: 999 }
   validates :password,   presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/}
 end
