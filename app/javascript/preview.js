56d28cd0a9a7aabcfb44427a069b60f0da516333
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-15 07:32:44 +0900

    プレビュー機能追加

---
+++ app/javascript/preview.js
@@ -0,0 +1,33 @@
+document.addEventListener('turbo:load', function(){
+  const itemNew = document.getElementById('item_form');
+
+  const previewList = document.getElementById('previews');
+
+  if (!itemNew) return null;
+
+    //input[type="file"]セレクタは、HTMLフォーム内のファイル入力フィールドをターゲットにする
+    //item[image]はitemデータのimageカラムを指す
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change',function(e){
+
+    const alreadyPreview = document.querySelector('.preview-image');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    }
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview-image');
+    
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class','preview-image');
+    previewImage.setAttribute('src',blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+
+  })
+  
+});
