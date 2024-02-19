2594434315ae5cae4a960036db0579703a31c277
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-19 13:37:50 +0900

    複数検索機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    if params[:q]&.dig(:product_name)
+      squished_keywords =params[:q][:product_name].squish
+      params[:q][:product_name_cont_any] = squished_keywords.split(" ")
+    end
     @user = User.find(params[:id])
     @q = @user.items.ransack(params[:q])  
     @items = @q.result
