c1724b7b3d28a6dc44785b69a4316bacb867d818
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-25 19:23:59 +0900

    slick追加

--- app/javascript/slide.js
+++ app/javascript/slide.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   $('.slider').slick({
-
+    accessibility: false,
+    autoplay:true,
+    infinite:true,
+    autoplaySpeed:3000,
+    arrows: false,
+    slidesToScroll:1,
+    fade:true,
+    pauseOnHover:false,
   });
 });
