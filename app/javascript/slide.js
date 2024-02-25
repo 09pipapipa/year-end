3ae94aa4be05beddfd305dc0b327955b8cdf155a
09pipapipa <148035399+09pipapipa@users.noreply.github.com>
2024-02-26 07:29:24 +0900

    js turbo削除　jsブラウザバック作動のため

--- app/javascript/slide.js
+++ app/javascript/slide.js
@@ -1,3 +1,7 @@
+document.addEventListener("turbolinks:before-cache",function(){
+  $('.slider.slick-initialized').slick('unslick');
+})
+
 $(document).ready(function(){
   $('.slider').slick({
     accessibility: false,
